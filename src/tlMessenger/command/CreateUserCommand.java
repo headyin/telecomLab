66b1cecef6c1df69723e8f474634b82444071bad
Xinshang Yin <xinshang.yin@mail.mcgill.ca>
2014-03-01 12:42:56 -0500

    handle password reading

--- src/tlMessenger/command/CreateUserCommand.java
+++ src/tlMessenger/command/CreateUserCommand.java
@@ -4,6 +4,7 @@
 package tlMessenger.command;
 
 import tlMessenger.data.Message;
+import tlMessenger.ui.CommandLineReader;
 
 /**
  * @author headyin
@@ -17,6 +18,19 @@ public class CreateUserCommand extends Command {
 
 	@Override
 	public Message execute(String parameter) {
+		String userName = CommandLineReader.getInstance().readLine("user name:");
+		String password = CommandLineReader.getInstance().readLine("password:",
+				new Character('*'));
+		String passwordConfirm = CommandLineReader.getInstance().readLine("password confirm:",
+				new Character('*'));
+		while (! password.equals(passwordConfirm)) {
+			System.out.println("Password doesn't not match, please enter again.");
+			password = CommandLineReader.getInstance().readLine("password:",
+					new Character('*'));
+			passwordConfirm = CommandLineReader.getInstance().readLine("password confirm:",
+					new Character('*'));
+		}
+		//System.out.println(userName + ", " + password);
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/tlMessenger/command/LoginCommand.java
+++ src/tlMessenger/command/LoginCommand.java
@@ -4,6 +4,8 @@
 package tlMessenger.command;
 
 import tlMessenger.data.Message;
+import tlMessenger.data.UserInfo;
+import tlMessenger.ui.CommandLineReader;
 
 /**
  * @author Xinshang, Chandani
@@ -17,6 +19,12 @@ public class LoginCommand extends Command {
 
 	@Override
 	public Message execute(String parameter) {
+		String userName = CommandLineReader.getInstance().readLine("user name:");
+		String password = CommandLineReader.getInstance().readLine("password:",
+				new Character('*'));
+		//System.out.println(userName + ", " + password);
+		UserInfo.getInstance().setUserName(userName);
+		UserInfo.getInstance().setPassword(password);
 		// TODO Auto-generated method stub
 		return null;
 	}
