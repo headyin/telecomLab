1a69acf0ae6afdf4c2c41f8242bcc3f234ab257e
Xinshang Yin <xinshang.yin@mail.mcgill.ca>
2014-03-01 13:10:53 -0500

    implement MessageType

--- src/tlMessenger/data/Message.java
+++ src/tlMessenger/data/Message.java
@@ -3,10 +3,166 @@
  */
 package tlMessenger.data;
 
+import java.io.ByteArrayInputStream;
+
 /**
  * @author Xinshang, Chandani
  *
  */
 public class Message {
+	/**
+	 * Create a message object with specified binary message
+	 * @param binaryMessage
+	 */
+	public Message(byte[] binaryMessage) {
+		this.binaryFormatMessage = new byte[MAX_MESSAGE_SIZE];
+		if (binaryMessage != null) {
+			for (int i = 0; i < binaryMessage.length; i++) {
+				this.binaryFormatMessage[i] = binaryMessage[i];
+			}
+		}
+		this.disassembleMessage();
+		this.fieldChanged = false;
+	}
+	
+	/**
+	 * create a message object with message type, sub type and data
+	 * @param messageType
+	 * @param subMessageType
+	 * @param data
+	 */
+	public Message(MessageType messageType, int subMessageType, String data) {
+		if (messageType == null) {
+			messageType = MessageType.BADLY_FORMATTED_MESSAGE;
+		}
+		if (data == null) {
+			data = "";
+		}
+		this.binaryFormatMessage = new byte[MAX_MESSAGE_SIZE];
+		this.messageType = messageType;
+		this.subMessageType = subMessageType;
+		this.data = new String(data.substring(0, MAX_DATA_LENGTH));
+		this.size = this.data.length();
+		this.ressambleMessage();
+		this.fieldChanged = false;
+	}
+	
+	/**
+	 * the maximum packet size is 262157 bytes
+	 */
+	private final int MAX_MESSAGE_SIZE  = 262156;
+	
+	/**
+	 * the maximum data size
+	 */
+	private final int MAX_DATA_LENGTH = 256144;
+	
+	private boolean fieldChanged;
+	
+	/**
+	 * The entire message in binary format
+	 */
+	byte[] binaryFormatMessage;
+	
+	/**
+	 * Message Type field
+	 */
+	private MessageType messageType;
+	
+	/**
+	 * Sub-message type field
+	 */
+	private int subMessageType;
+	
+	/**
+	 * message size field
+	 */
+	private int size;
+	
+	/**
+	 * message data field
+	 */
+	private String data;
+	
+	/**
+	 * @return the messageType
+	 */
+	public MessageType getMessageType() {
+		return messageType;
+	}
+	/**
+	 * @param messageType the messageType to set
+	 */
+	public void setMessageType(MessageType messageType) {
+		this.messageType = messageType;
+		this.fieldChanged = true;
+	}
+	/**
+	 * @return the subMessageType
+	 */
+	public int getSubMessageType() {
+		return subMessageType;
+	}
+	/**
+	 * @param subMessageType the subMessageType to set
+	 */
+	public void setSubMessageType(int subMessageType) {
+		this.subMessageType = subMessageType;
+		this.fieldChanged = true;
+	}
+	/**
+	 * @return the size
+	 */
+	public int getDataSize() {
+		return size;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public String getData() {
+		return data;
+	}
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(String data) {
+		this.data = new String(data);
+		this.fieldChanged = true;
+	}
+	
+	/**
+	 * @return the binaryFormatMessage
+	 */
+	public byte[] getBinaryFormatMessage() {
+		if (this.fieldChanged) {
+			this.ressambleMessage();
+		}
+		return binaryFormatMessage;
+	}
+	/**
+	 * @param binaryFormatMessage the binaryFormatMessage to set
+	 */
+	public void setBinaryFormatMessage(byte[] binaryFormatMessage) {
+		this.binaryFormatMessage = binaryFormatMessage;
+		this.disassembleMessage();
+	}
+	
+	/**
+	 * reassemble the message to binary format
+	 */
+	private void ressambleMessage() {
+		this.size = data.length();
+		//TODO:
+		this.fieldChanged = false;
+	}
+	
+	/**
+	 * disassemble the binary format message to the 4 fields
+	 */
+	private void disassembleMessage() {
+		//TODO:
+		this.fieldChanged = false;
+	}
 
 }
--- src/tlMessenger/data/MessageType.java
+++ src/tlMessenger/data/MessageType.java
@@ -8,5 +8,66 @@ package tlMessenger.data;
  *
  */
 public enum MessageType {
+	EXIT(0),
+	BADLY_FORMATTED_MESSAGE(1),
+	ECHO(2),
+	LOGIN(3),
+	LOGOFF(4),
+	CREATE_USER(5),
+	DELETE_USER(6),
+	CREATE_STORE(7),
+	SEND_MESSAGE(8),
+	QUERY_MESSAGE(9);
 
+	public static final int EXIT_VALUE = 0;
+	public static final int BADLY_FORMATTED_MESSAGE_VALUE = 1;
+	public static final int ECHO_VALUE = 2;
+	public static final int LOGIN_VALUE = 3;
+	public static final int LOGOFF_VALUE = 4;
+	public static final int CREATE_USER_VALUE = 5;
+	public static final int DELETE_USER_VALUE = 6;
+	public static final int CREATE_STORE_VALUE = 7;
+	public static final int SEND_MESSAGE_VALUE = 8;
+	public static final int QUERY_MESSAGE_VALUE = 9;
+	/**
+	 * the value of message type
+	 */
+	private final int value;
+	
+	/**
+	 * get the value of the message type
+	 * @return
+	 */
+	public int getValue() {
+		return this.value;
+	}
+	
+	/**
+	 * get the message type by value
+	 * @param value
+	 * @return message type
+	 */
+	public static MessageType getByValue(int value) {
+		switch (value) {
+		case EXIT_VALUE: return EXIT;
+		case BADLY_FORMATTED_MESSAGE_VALUE: return BADLY_FORMATTED_MESSAGE;
+		case ECHO_VALUE: return ECHO;
+		case LOGIN_VALUE: return LOGIN;
+		case LOGOFF_VALUE: return LOGOFF;
+		case CREATE_USER_VALUE: return CREATE_USER;
+		case DELETE_USER_VALUE: return DELETE_USER;
+		case CREATE_STORE_VALUE: return CREATE_STORE;
+		case SEND_MESSAGE_VALUE: return SEND_MESSAGE;
+		case QUERY_MESSAGE_VALUE: return QUERY_MESSAGE;
+		default: return null;
+		}
+	}
+	
+	/**
+	 * private constructor
+	 * @param value
+	 */
+	private MessageType(int value) {
+		this.value = value;
+	}
 }
